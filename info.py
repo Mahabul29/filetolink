9872b5cdee5836a96accf740eadcec295ec7bb11
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-26 23:53:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,21 +1,15 @@
 import os
-from os import environ, getenv
+from os import getenv
 
 class Var(object):
-    name = str(getenv('name', 'File_To_Link_Bot'))
     API_ID = int(getenv('API_ID', '0'))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
+    API_HASH = getenv('API_HASH', '')
+    BOT_TOKEN = getenv('BOT_TOKEN', '')
+    DATABASE_URL = getenv('DATABASE_URL', '')
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '0'))
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
     
-    # Web Server Config
+    # Koyeb Config
     PORT = int(getenv('PORT', '8080'))
     FQDN = getenv('FQDN', '0.0.0.0')
-    HAS_SSL = str(getenv('HAS_SSL', 'True')).lower() in ('true', '1', 'yes')
+    HAS_SSL = getenv('HAS_SSL', 'True').lower() in ('true', '1', 'yes')
     
-    if HAS_SSL:
-        URL = f"https://{FQDN.rstrip('/')}/"
-    else:
-        URL = f"http://{FQDN.rstrip('/')}:{PORT}/"
-        
