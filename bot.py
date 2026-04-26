c5f7f35eff962bb9255fd935ec44c0d078dd03f4
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-26 23:34:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,32 @@
 from pyrogram import Client
 from info import Var
+from web.server import web_server
+from aiohttp import web
+import asyncio
 
-# This starts the bot using the clean variables
-bot = Client(
-    name=Var.name,
-    api_id=Var.API_ID,
-    api_hash=Var.API_HASH,
-    bot_token=Var.BOT_TOKEN,
-    workers=Var.WORKERS
-)
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name=Var.name,
+            api_id=Var.API_ID,
+            api_hash=Var.API_HASH,
+            bot_token=Var.BOT_TOKEN,
+            workers=Var.WORKERS
+        )
+
+    async def start(self):
+        await super().start()
+        print("Bot Started!")
+        
+        # Start Web Server for Koyeb
+        app = await web_server()
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, '0.0.0.0', Var.PORT)
+        await site.start()
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot Stopped!")
+
+Bot().run()
