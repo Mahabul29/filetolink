0e06e020766fc5f53a4251f9808d140320c5fa5d
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-26 23:41:06 +0530

    Create file_size.py

---
+++ utils/file_size.py
@@ -0,0 +1,9 @@
+def humanbytes(size):
+    if not size:
+        return "0 B"
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:.2f} {unit}"
+  
