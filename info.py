098e42934d69435905f517a656be24614d26041f
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-27 00:06:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,7 +6,7 @@ class Var(object):
     API_HASH = "679f843b6d9485bead1b81852a9634f4"
     BOT_TOKEN = "8612139825:AAGe7npb_imXopdOhXXRbSwnreDp3_HwnVo"
     
-    # Database & Essential Logs
+    # Database & Logs
     DATABASE_URL = "mongodb+srv://mahabul201:Mahbul288550@mahabul201.83adx.mongodb.net/?retryWrites=true&w=majority&appName=Mahabul201"
     BIN_CHANNEL = -1002384901959
     LOG_CHANNEL = -1002485776908
@@ -15,6 +15,10 @@ class Var(object):
     OWNER_ID = 5733685945
     name = "FileToLinkBot"
     
+    # --- ADD THIS LINE TO FIX THE ERROR ---
+    WORKERS = 4 
+    # ---------------------------------------
+
     # Koyeb Web Settings
     PORT = 8080
     FQDN = "faint-darya-mahavayst-636a2272.koyeb.app"
