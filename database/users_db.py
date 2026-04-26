d5a29f980342136f0433a2cde38984272d7e079c
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-26 23:32:59 +0530

    Update users_db.py

--- database/users_db.py
+++ database/users_db.py
@@ -8,8 +8,15 @@ class Database:
         self.col = self.db.users
 
     async def add_user(self, id):
-        user = {'id': id}
+        user = dict(id=id)
         if not await self.col.find_one({'id': id}):
             await self.col.insert_one(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        return self.col.find({})
 
 db = Database(Var.DATABASE_URL, Var.name)
