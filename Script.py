d3cf369252a70a0430dd1a3e540b7dd450da4941
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-26 23:30:35 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,9 @@
+class script(object):
+    START_MSG = """<b>Hello {}!!</b>
+
+I am a Telegram File to Link Bot. I can provide direct high-speed download links for Google Chrome and other browsers.
+
+<b>Just send me any file to get started!</b>"""
+    
+    CAPTION = "<b>File Name:</b> <code>{file_name}</code>\n<b>Size:</b> <code>{file_size}</code>"
+  
