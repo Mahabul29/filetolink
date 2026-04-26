a5665b2a094b2245569b065b3f780c84fcb4870d
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-26 23:32:14 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,9 +1,11 @@
 class script(object):
     START_MSG = """<b>Hello {}!!</b>
 
-I am a Telegram File to Link Bot. I can provide direct high-speed download links for Google Chrome and other browsers.
+I am a Telegram File to Link Bot. Send me any file, and I will give you a direct high-speed download link for Chrome.
 
-<b>Just send me any file to get started!</b>"""
+<b>Maintained by:</b> @SatyajeetKumarOfficial"""
+
+    CAPTION = """<b>📁 File Name:</b> <code>{file_name}</code>
+<b>📦 Size:</b> <code>{file_size}</code>
+<b>🔗 Link:</b> <code>{link}</code>"""
     
-    CAPTION = "<b>File Name:</b> <code>{file_name}</code>\n<b>Size:</b> <code>{file_size}</code>"
-  
