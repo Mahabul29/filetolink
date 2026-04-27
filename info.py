01c410724cb0be3952bdc47044e9308259ff8893
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-27 17:13:49 +0530

    Delete info.py

--- info.py
+++
@@ -1,9 +0,0 @@
-import os
-
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
-BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "0"))  # private channel to store files
-FQDN = os.environ.get("FQDN", "")  # your Koyeb app URL e.g. myapp.koyeb.app
-PORT = int(os.environ.get("PORT", "8080"))
