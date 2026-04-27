dd1917e8fcc02bf2185f56549cf2b6e2ddc22a04
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-27 16:30:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,31 +1,9 @@
 import os
 
-class Var(object):
-    # API Credentials
-    API_ID = 23787292
-    API_HASH = "679f843b6d9485bead1b81852a9634f4"
-    BOT_TOKEN = "8612139825:AAGe7npb_imXopdOhXXRbSwnreDp3_HwnVo"
-    
-    # Database & Logs
-    DATABASE_URL = "mongodb+srv://mahabul201:Mahbul288550@mahabul201.83adx.mongodb.net/?retryWrites=true&w=majority&appName=Mahabul201"
-    BIN_CHANNEL = -1002384901959
-    LOG_CHANNEL = -1002485776908
-    
-    # Admin Info & Identity
-    OWNER_ID = 5733685945
-    name = "FileToLinkBot"
-    
-    # --- ADD THIS LINE TO FIX THE ERROR ---
-    WORKERS = 4 
-    # ---------------------------------------
-
-    # Koyeb Web Settings
-    PORT = 8080
-    FQDN = "faint-darya-mahavayst-636a2272.koyeb.app"
-    URL = f"https://{FQDN}/"
-    
-    # Simple Switches
-    FSUB = False
-    VERIFY = False
-    HAS_SSL = True
-    
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
+BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "0"))  # private channel to store files
+FQDN = os.environ.get("FQDN", "")  # your Koyeb app URL e.g. myapp.koyeb.app
+PORT = int(os.environ.get("PORT", "8080"))
