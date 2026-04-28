e66b44b8d71022ce34503fd5de9b688e1b851c4c
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-29 01:28:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import asyncio
-from bot import bot
-from pyrogram import idle
-
-async def main():
-    await bot.start()
-    print("Keep this terminal open to keep the bot alive.")
-    await idle() # This stops the script from closing immediately
+from bot import Bot  # Capital 'B' to match the class name
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    Bot().run()
     
