35f6c6bd552ece98e82270f7b29f66af6f3f0318
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-29 01:07:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Mandatory Variables
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# Point both to your channel ID (e.g., -1002485776908)
-BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "0"))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", os.environ.get("BIN_CHANNEL", "0")))
+# Unified Storage (LOG_CHANNEL = BIN_CHANNEL)
+# Make sure this is -1002485776908 in Koyeb
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", LOG_CHANNEL))
 
-# Connection and Database
+# Connection & Branding
 FQDN = os.environ.get("FQDN", "localhost")
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
