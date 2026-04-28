7fd4f5167c7e5d7764427a99b25a6f6f96be36c9
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-28 23:42:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,11 @@ load_dotenv()
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# This was the missing link!
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100xxxxxxxxxx")) 
+
 BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "0"))
-# For the download links
 FQDN = os.environ.get("FQDN", "localhost") 
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-# Database
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
