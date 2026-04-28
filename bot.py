5a1a8268e5d6a67a1ba43b89f1e86c62e37cf332
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-28 23:26:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 from pyrogram import Client
 import os
 
-bot = Client(
-    "my_bot_session",
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH"),
-    bot_token=os.environ.get("BOT_TOKEN"),
-    plugins=dict(root="plugins")  # <--- THIS LINE IS CRITICAL
-)
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="filetolink",
+            api_id=int(os.environ.get("API_ID")),
+            api_hash=os.environ.get("API_HASH"),
+            bot_token=os.environ.get("BOT_TOKEN"),
+            plugins=dict(root="plugins") # THIS TELLS THE BOT WHERE THE CODE IS
+        )
+
+bot = Bot()
