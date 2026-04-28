01e7919af248f9a30d910565db1cee8c192e5b9f
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-28 11:38:32 +0530

    Update files.py

--- plugins/files.py
+++ plugins/files.py
@@ -1,23 +1,24 @@
+
 import logging
 from pyrogram import Client, filters
-from config import LOG_CHANNEL, FQDN  # Using FQDN to match your variables
+from config import LOG_CHANNEL, FQDN
 
 logger = logging.getLogger(__name__)
 
-# THIS DECORATOR IS THE TRIGGER:
-@Client.on_message(filters.private & (filters.document | filters.video | filters.audio | filters.photo))
+# This line triggers the code when you send a file
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio))
 async def file_handler(client, message):
     try:
-        # 1. Copy file to your storage channel
+        # 1. Copy the file to your storage channel
         msg = await message.copy(chat_id=LOG_CHANNEL)
         
-        # 2. Build the link
-        # We ensure https:// is present and use FQDN from your config
+        # 2. Generate the link
+        # We use FQDN and the message ID from the log channel
         file_id = msg.id
         
-        # Clean the FQDN to prevent double slashes
-        clean_fqdn = FQDN.strip('/')
-        download_link = f"https://{clean_fqdn}/dl/{file_id}"
+        # Clean FQDN just in case there's a typo in your variables
+        clean_host = FQDN.strip("/")
+        download_link = f"https://{clean_host}/dl/{file_id}"
 
         await message.reply_text(
             f"<b>✅ Your Download Link:</b>\n"
