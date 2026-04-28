c0137d6cf2f9d00e236c37d90ed6537898b266e9
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-04-28 23:29:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,17 @@ class Bot(Client):
             api_id=int(os.environ.get("API_ID")),
             api_hash=os.environ.get("API_HASH"),
             bot_token=os.environ.get("BOT_TOKEN"),
-            plugins=dict(root="plugins") # THIS TELLS THE BOT WHERE THE CODE IS
+            workers=50,
+            plugins=dict(root="plugins"), # THIS IS THE PART YOU ARE LIKELY MISSING
+            sleep_threshold=5
         )
+
+    async def start(self):
+        await super().start()
+        print("Bot started successfully")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot stopped")
 
 bot = Bot()
