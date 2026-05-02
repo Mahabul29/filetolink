5fbc4c9d8000f415f4a11815272e2f5ab5a05384
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-03 00:48:56 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -4,19 +4,51 @@ from plugins.utils.markup import Buttons
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
-    if query.data == "about":
-        await query.message.edit_caption(
-            caption="‣ <b>ᴍʏ ɴᴀᴍᴇ :</b> ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ\n‣ <b>ʟɪʙʀᴀʀʏ :</b> ᴘʏʀᴏɢʀᴀᴍ\n‣ <b>ᴄʀᴇᴀᴛᴇᴅ ʙʏ :</b> Moon",
-            reply_markup=Buttons.BACK_BUTTON
+    data = query.data
+
+    if data == "about":
+        # Updated to match the "About" style in your screenshot
+        about_text = (
+            "▶ <b>MY NAME :</b> SHORTLINK BOT\n"
+            "▶ <b>LIBRARY :</b> PYROGRAM\n"
+            "▶ <b>DATABASE :</b> MONGODB\n"
+            "▶ <b>LANGUAGE :</b> PYTHON 3\n"
+            "▶ <b>BOT SERVER :</b> KOYEB\n"
+            "▶ <b>CREATED BY :</b> Moon"
         )
-    elif query.data == "help":
         await query.message.edit_caption(
-            caption="<b>ʜᴇʟᴘ ᴍᴇɴᴜ:</b>\nJust send me any file or media, and I will generate a direct download link for you instantly!",
-            reply_markup=Buttons.BACK_BUTTON
+            caption=about_text,
+            reply_markup=Buttons.BACK_CLOSE_BUTTONS
         )
-    elif query.data == "back":
+
+    elif data == "help":
+        # Modified Help Menu
+        help_text = (
+            "<b>💡 How to Use Me?</b>\n\n"
+            "1️⃣ Add me to your channel or send a file here.\n"
+            "2️⃣ I will provide a high-speed download link.\n"
+            "3️⃣ Use the 'Bot' button to share the link with others!\n\n"
+            "<i>Everything is automated and instant.</i>"
+        )
         await query.message.edit_caption(
-            caption="👋 <b>Hey!</b>\n\nSend any file or media to get a direct download link.",
+            caption=help_text,
+            reply_markup=Buttons.BACK_CLOSE_BUTTONS
+        )
+
+    elif data == "back_to_start":
+        # Returns to the original Start Message
+        start_caption = (
+            "👋 <b>Hey Moon!</b>\n\n"
+            "I can convert your files into high-speed direct links.\n\n"
+            "• Direct Download Link\n"
+            "• Fast Streaming Support\n\n"
+            "Just send any file now 👇"
+        )
+        await query.message.edit_caption(
+            caption=start_caption,
             reply_markup=Buttons.START_BUTTONS
         )
-        
+
+    elif data == "close":
+        # Deletes the message completely
+        await query.message.delete()
