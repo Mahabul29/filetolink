44378a64325ded6fdc9a542e2811c08eeeb3d5d7
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-02 23:55:09 +0530

    Update markup.py

--- plugins/utils/markup.py
+++ plugins/utils/markup.py
@@ -1,19 +1,19 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Buttons:
-    # Main Menu
+    # The Main Start Menu
     START_BUTTONS = InlineKeyboardMarkup([
         [
             InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about"),
             InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help")
         ],
         [
-            InlineKeyboardButton("ᴅᴇᴠʟᴏᴘᴇʀ", url="https://t.me/Mahabul201")
+            InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Mahabul201")
         ]
     ])
 
-    # About Page Back Button
+    # The Back button used in About/Help pages
     BACK_BUTTON = InlineKeyboardMarkup([
         [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="back")]
     ])
-  
+    
