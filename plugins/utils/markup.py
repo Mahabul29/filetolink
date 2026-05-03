a0bb2f481eb2f58fa190f7e46b2e1e801a13cbf3
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-03 09:12:00 +0530

    Update markup.py

--- plugins/utils/markup.py
+++ plugins/utils/markup.py
@@ -1,18 +1,14 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Buttons:
-    # Main Menu side-by-side
     START_BUTTONS = InlineKeyboardMarkup([
         [
             InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="about"),
             InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help")
         ],
-        [
-            InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Mahabul201")
-        ]
+        [InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Mahabul201")]
     ])
 
-    # Side-by-Side Download/Bot links
     @staticmethod
     def file_links(download_link, bot_link):
         return InlineKeyboardMarkup([
@@ -22,7 +18,6 @@ class Buttons:
             ]
         ])
 
-    # Side-by-Side Back and Close
     BACK_CLOSE_BUTTONS = InlineKeyboardMarkup([
         [
             InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="back_to_start"),
