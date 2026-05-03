592ef606deaca852eaa207efbe1089e3936e8702
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-03 08:55:08 +0530

    Update database.py

--- plugins/utils/database.py
+++ plugins/utils/database.py
@@ -3,17 +3,13 @@ from config import DATABASE_URI
 
 class Database:
     def __init__(self):
-        # Connects to your MongoDB using the URI in Koyeb
         self.client = AsyncIOMotorClient(DATABASE_URI)
         self.db = self.client["FileToLinkBot"]
         self.users = self.db["users"]
 
     async def add_user(self, user_id):
-        user = {"_id": user_id}
-        try:
-            await self.users.insert_one(user)
-        except:
-            pass # User already exists in DB
+        try: await self.users.insert_one({"_id": user_id})
+        except: pass
 
     async def get_all_users(self):
         return self.users.find({})
@@ -21,5 +17,4 @@ class Database:
     async def total_users_count(self):
         return await self.users.count_documents({})
 
-# This is the "db" object start.py is looking for
 db = Database()
