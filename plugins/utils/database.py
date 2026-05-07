ed39ac4b6522e3654336fda1bdcab58a356e7a0c
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-08 00:06:49 +0530

    Update database.py

--- plugins/utils/database.py
+++ plugins/utils/database.py
@@ -8,12 +8,16 @@ class Database:
         self.users = self.db["users"]
 
     async def add_user(self, user_id):
-        try:
-            await self.users.insert_one({"_id": user_id})
-        except:
-            pass 
+        # Using update_one with upsert=True is cleaner than try/except
+        # because it won't throw an error if the user already exists.
+        await self.users.update_one(
+            {"_id": user_id},
+            {"$set": {"_id": user_id}},
+            upsert=True
+        )
 
     async def get_all_users(self):
+        # This returns a cursor that you can use in an 'async for' loop
         return self.users.find({})
 
     async def total_users_count(self):
