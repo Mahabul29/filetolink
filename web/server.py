b8394f80effe2352b2a617bb854d3f3ee3789349
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-10 19:28:49 +0530

    Update server.py

--- web/server.py
+++ web/server.py
@@ -2,21 +2,25 @@ import logging
 from aiohttp import web
 from config import PORT
 from web.stream import video_player, stream_handler, download_handler
+from web.play import play_handler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 async def home(request):
     return web.Response(text="✅ Bot is Running!")
+
 
 async def web_server(bot_client):
     app = web.Application(client_max_size=30 * 1024 * 1024)
     app["bot_client"] = bot_client
 
     app.router.add_get("/", home)
-    app.router.add_get("/watch/{file_id}", video_player)   # Stream page (HTML player)
-    app.router.add_get("/stream/{file_id}", stream_handler) # Raw stream for video tag
-    app.router.add_get("/dl/{file_id}", download_handler)   # Force download
+    app.router.add_get("/watch/{file_id}", video_player)
+    app.router.add_get("/stream/{file_id}", stream_handler)
+    app.router.add_get("/dl/{file_id}", download_handler)
+    app.router.add_get("/play/{player}/{file_id}", play_handler)
 
     runner = web.AppRunner(app)
     await runner.setup()
