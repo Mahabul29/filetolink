d38bd0e69f9e81f4812783bae691ac834431b6c9
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-10 19:29:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from web.server import web_server
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 class Bot(Client):
     def __init__(self):
         super().__init__(
