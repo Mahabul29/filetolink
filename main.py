0bdde3fb5794f2d7dce6aacbaa39b12515077efc
Mahabul201 <Mahabulbhai288550@gmail.com>
2026-05-10 17:40:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from bot import Bot
 async def main():
     bot = Bot()
     await bot.start()
-    # Keeps the bot running indefinitely
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
